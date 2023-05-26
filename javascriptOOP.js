5a2649969c6c6fc887a6a458aa120357f3507730
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-05-26 16:20:44 +0300

    OOP PRACTICE

--- javascriptOOP.js
+++ javascriptOOP.js
@@ -25,7 +25,7 @@ Book.prototype.revise = function(newYear) {
 }
 
 const book1 = new Book("BOOk1", "John Doe", "2013");
-const book2 = new Book("BOOK2", "Festo", "2020");
+
 
 
 
@@ -40,4 +40,30 @@ Magazine.prototype = Object.create(Book.prototype);
 
 //Instantiate Magazine Object
  const mag1 = new Magazine('Mag1', "Musunza", "2003", "March");
- alert(mag1.getSummary());
+ 
+
+ //ES6 classes
+
+ class Book {
+    constructor(title, author, year){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    getSummary() {
+        return `${this.title} was written by ${this.author} in ${this.year}`;
+    };
+    getAge() {
+        const years = new Date().getFullYear() - this.year;
+        return `${this.title} is ${years} years old`;
+    };
+    revise(newYear) {
+        this.year = newYear;
+        this.revised = true;
+    }
+    static topBookStore() {
+        return 'Daniel & Festo';
+    }
+ }
+ const book2 = new Book("BOOK2", "Festo", "2020");
