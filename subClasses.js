7b0d24abb2fd1a3f5418d9cd23d83d7f385b79f2
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-05-26 16:41:40 +0300

    OOP PRACTICE

---
+++ subClasses.js
@@ -0,0 +1,20 @@
+class Book {
+    constructor(title, author, year){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    getSummary() {
+        return `${this.title} was written by ${this.author} in ${this.year}`;
+    };
+}
+
+class Magazine extends Book {
+    constructor(title, author, year, month){
+    super(title, author, year);
+    this.month = month;
+    }
+}
+const mag1 = new Magazine("mg", "festo", "2000", "June");
+alert(mag1.getSummary());
